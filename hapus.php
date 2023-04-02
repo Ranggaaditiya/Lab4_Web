5aa1e9315ac12d0eef217d9822a22b3fecb6d472
Raditya <127511355+Ranggaaditiya@users.noreply.github.com>
2023-04-03 05:57:37 +0700

    Add files via upload

---
+++ hapus.php
@@ -0,0 +1,8 @@
+<?php
+include('header.php');
+?>
+$id = $_GET['id'];
+$sql = "DELETE FROM data_barang WHERE id_barang = '{$id}'";
+$result = mysqli_query($conn, $sql);
+header('location: index.php');
+?>
